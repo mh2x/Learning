6fa2f8152bc0f820d06fdc4dd3186f35e4f12234
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-14 18:15:47 +0300

    Added better UX for jobs with employer name

--- PHP/Herd/example1/resources/views/jobs.blade.php
+++ PHP/Herd/example1/resources/views/jobs.blade.php
@@ -6,7 +6,12 @@
         @foreach ($jobs as $job )
         <a class="block px-4 py-6 border border-gray-200 rounded-lg"
             href="/jobs/{{$job['id']}}">
-            <strong>{{$job['title']}} </strong> : Makes <strong>{{$job['salary']}}</strong> per year.
+            <div class="font-bold text-blue-500 text-sm">
+                {{$job->employer->name??"NA"}}
+            </div>
+            <div>
+                <strong>{{$job['title']}} </strong> : Makes <strong>{{$job['salary']}}</strong> per year.
+            </div>
         </a>
         @endforeach
     </div>
