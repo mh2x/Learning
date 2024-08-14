bae9abc7a19b5f4f1698c15a5bae7278e8b34669
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-14 03:00:31 +0300

    make $jobs as global and pass to functions with use($jobs)

--- PHP/Herd/example1/routes/web.php
+++ PHP/Herd/example1/routes/web.php
@@ -3,31 +3,34 @@
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Support\Arr;
 
+//define a global array
+//you can pass it to functions (closures) with use()
+$jobs = [
+    [
+        'id' => '1',
+        'title' => 'Director',
+        'salary' => '$50,000'
+    ],
+    [
+        'id' => '2',
+        'title' => 'Programmer',
+        'salary' => '$20,000'
+    ],
+    [
+        'id' => '3',
+        'title' => 'Teacher',
+        'salary' => '$40,000'
+    ]
+];
+
+
 Route::get('/', function () {
     return view('home', [
         'greeting' => 'Hello'
     ]);
 });
 
-Route::get('/jobs', function () {
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => '2',
-            'title' => 'Programmer',
-            'salary' => '$20,000'
-        ],
-        [
-            'id' => '3',
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
-
+Route::get('/jobs', function () use ($jobs) {
     return view(
         'jobs',
         [
@@ -36,26 +39,7 @@ Route::get('/jobs', function () {
     );
 });
 
-Route::get('/jobs/{id}', function ($id) {
-
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => '2',
-            'title' => 'Programmer',
-            'salary' => '$20,000'
-        ],
-        [
-            'id' => '3',
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
-
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     //instead of foreach here, use laravel helper Arr for arrays
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
