b8baacbf8ca89b8b766a3c10d0b919c854dff064
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-14 03:07:43 +0300

    zxczx

--- PHP/Herd/example1 - Copy/resources/views/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact
-    </x-slot:heading>
-    <h1>Hello from contact page</h1>
-</x-layout>
--- PHP/Herd/example1 - Copy/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home
-    </x-slot:heading>
-    <h1>Home Page</h1>
-    <h2>{{$greeting}}</h2>
-</x-layout>
