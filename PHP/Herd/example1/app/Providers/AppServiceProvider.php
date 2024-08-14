05237f4086abaa0004eef8e9b2646e246c2b44e4
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-14 19:11:05 +0300

    configure paginator to use bootstrap

--- PHP/Herd/example1/app/Providers/AppServiceProvider.php
+++ PHP/Herd/example1/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,11 +17,13 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Boot strap any application services.
      */
     public function boot(): void
     {
         //Triggered after all project dependencies have successfully been loaded
         Model::preventLazyLoading();
+
+        Paginator::useBootstrapFive();
     }
 }
