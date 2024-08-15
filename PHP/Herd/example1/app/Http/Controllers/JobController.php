ac9a3a23f6d6ea2d46e88c3177a8f9b5c11204e3
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-15 04:46:42 +0300

    Adding JobController and simplifying web.php routes --> code cleanup and better approach with controllers

---
+++ PHP/Herd/example1/app/Http/Controllers/JobController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    //
+    public function index()
+    {
+        //NOTE: employer is the relation found inside the Job class
+        //This fixes N+1 Problem for Select * from Employer where id=?
+        //However, this doesn't scale if you have millions of records!
+        //We will need pagination
+
+        //This is called EAGER LOADING --> i.e. [Select * from Employer]!!!
+        //We will need a pagination method soon...
+        //$jobs = Job::with('employer')->get();
+
+        //Use CURSOR pagination ==> BEST PERFORMANCE!!
+        //latest --> order by created desc
+        $jobs = Job::with('employer')->latest()->simplePaginate(10);
+        return view('jobs.index', ['jobs' => $jobs]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+    public function show(Job $job)
+    {
+        //dd($job);   //dump and die!
+        return view('jobs.show', ['job' => $job]);
+    }
+    public function edit(Job $job)
+    {
+        //dd($job);   //dump and die!
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    public function update(Job $job)
+    {
+        //dd($job);   //dump and die!
+
+        // validate
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        // authorize (on hold...)
+        // update the job
+        $job->title = request('title');
+        $job->salary = request('salary');
+
+        // and persist
+        $job->save();
+        /*
+        We can also do this:
+
+        $Job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+    */
+
+        // redirect to the job page
+        return redirect("/jobs/$job->id");
+    }
+
+    public function destroy(Job $job)
+    {
+        // authorize (on hold...)
+        // delete the job
+        $job->delete();
+        //redirect
+        return redirect('/jobs');
+    }
+    public function store(Request $request)
+    {
+        //dd(request()->all());   //dump and die!
+
+        //Validation ....
+        //For more details on Laravel provided validations:
+        //see: https://laravel.com/docs/11.x/validation
+
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        //back to job list
+        return redirect('/jobs');
+    }
+}
--- PHP/Herd/example1/routes/web.php
+++ PHP/Herd/example1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use Mockery\Generator\Method;
 use App\Models\Job;
@@ -10,110 +11,6 @@ Route::get('/', function () {
     ]);
 });
 
-//Index
-Route::get('/jobs', function () {
-    //NOTE: employer is the relation found inside the Job class
-    //This fixes N+1 Problem for Select * from Employer where id=?
-    //However, this doesn't scale if you have millions of records!
-    //We will need pagination
-
-    //This is called EAGER LOADING --> i.e. [Select * from Employer]!!!
-    //We will need a pagination method soon...
-    //$jobs = Job::with('employer')->get();
-
-    //Use CURSOR pagination ==> BEST PERFORMANCE!!
-    //latest --> order by created desc
-    $jobs = Job::with('employer')->latest()->simplePaginate(10);
-
-    return view('jobs.index', ['jobs' => $jobs]);
-});
-
-//Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-//Route Model Binding
-//Route::get('/posts/{post:slug}', function (Post $post) {...}
-
-//Show
-Route::get('/jobs/{job}', function (Job $job) {
-    //dd($job);   //dump and die!
-    return view('jobs.show', ['job' => $job]);
-});
-
-
-//Store in DB
-Route::post('/jobs', function () {
-    //dd(request()->all());   //dump and die!
-
-    //Validation ....
-    //For more details on Laravel provided validations:
-    //see: https://laravel.com/docs/11.x/validation
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    //back to job list
-    return redirect('/jobs');
-});
-
-//Edit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    //dd($job);   //dump and die!
-    return view('jobs.edit', ['job' => $job]);
-});
-
-//update
-Route::patch('/jobs/{job}', function (Job $job) {
-
-    //dd($job);   //dump and die!
-
-    // validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    // authorize (on hold...)
-    // update the job
-    $job->title = request('title');
-    $job->salary = request('salary');
-
-    // and persist
-    $job->save();
-    /*
-        We can also do this:
-
-        $Job->update([
-            'title' => request('title'),
-            'salary' => request('salary'),
-            'employer_id' => 1,
-        ]);
-    */
-
-    // redirect to the job page
-    return redirect("/jobs/$job->id");
-});
-
-//delete or destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-    // authorize (on hold...)
-    // delete the job
-    $job->delete();
-    //redirect
-    return redirect('/jobs');
-});
-
-
 Route::get('/about', function () {
     return view('about');
 });
@@ -121,3 +18,16 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+//Route Model Binding
+//Route::get('/posts/{post:slug}', function (Post $post) {...}
+
+
+//============JOB ROUTES====================
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
