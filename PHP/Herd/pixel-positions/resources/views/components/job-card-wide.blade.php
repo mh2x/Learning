5979d0722f936d856f374d6b034ffc336df02b39
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-17 04:40:45 +0300

    many ui changes including 2 card components, employer logo, ...

---
+++ PHP/Herd/pixel-positions/resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<div
+    class="p-5 bg-white/5 rounded-xl flex flex-row gap-x-6 border border-transparent hover:border-blue-800 group transition-colors duration-300">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag1</x-tag>
+            <x-tag>Tag2</x-tag>
+            <x-tag>Tag3</x-tag>
+        </div>
+    </div>
+</div>
--- PHP/Herd/pixel-positions/resources/views/components/job-card.blade.php
+++ PHP/Herd/pixel-positions/resources/views/components/job-card.blade.php
@@ -1,8 +1,9 @@
-<div class="p-5 bg-white/5 rounded-xl flex flex-col text-center">
+<div
+    class="p-5 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
@@ -10,6 +11,7 @@
             <x-tag>Tag2</x-tag>
             <x-tag>Tag3</x-tag>
         </div>
-        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+        <x-employer-logo width="42" />
+
     </div>
 </div>
