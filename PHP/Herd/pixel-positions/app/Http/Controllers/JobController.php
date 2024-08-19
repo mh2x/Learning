6a7dc1de030a2194e885314084991fa20eef9728
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-19 07:55:05 +0300

    Modify route to go to JobController class Also removed welcome page and created index.blade.php

--- PHP/Herd/pixel-positions/app/Http/Controllers/JobController.php
+++ PHP/Herd/pixel-positions/app/Http/Controllers/JobController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
+use App\Models\Tag;
 
 class JobController extends Controller
 {
@@ -14,6 +15,10 @@ class JobController extends Controller
     public function index()
     {
         //
+        return view('jobs.index', [
+            'jobs' => Job::all(), //in real project, you would paginate this!
+            'tags' => Tag::all(),
+        ]);
     }
 
     /**
