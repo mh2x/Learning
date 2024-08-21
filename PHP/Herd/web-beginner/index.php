2ef79310f79e56caf49029dbe771a81e797286d9
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-22 01:30:33 +0300

    1st PDO query example on posts table

--- PHP/Herd/web-beginner/index.php
+++ PHP/Herd/web-beginner/index.php
@@ -3,3 +3,19 @@
 
 require_once("utils.php");
 include("router.php");
+
+//connect to our MySQL database using PDO
+
+//Data Source Name (DSN)
+$dsn = "mysql:host=localhost;port=3306;dbname=webbeginner;user=root;password=Mh2x@WLM;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+//prepare a new query
+$stmt = $pdo->prepare("select * from posts");
+$stmt->execute();
+
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
