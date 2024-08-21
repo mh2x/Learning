57dde55d519e75188dcb1d28082b85df24cea0a8
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-21 03:03:58 +0300

    var_dump($_SERVER);

--- PHP/Herd/web-beginner/views/index.view.php
+++ PHP/Herd/web-beginner/views/index.view.php
@@ -6,6 +6,9 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="font-bold text-lg">Hello, Welcome to my home page.</h1>
+
+        <pre><?php var_dump($_SERVER); ?></pre>
+
     </div>
 </main>
 
