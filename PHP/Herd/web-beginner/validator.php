2a4c685ad9234859dae196946c64eb9599b8a0db
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-23 01:59:32 +0300

    added email validator using filter_var php function

--- PHP/Herd/web-beginner/validator.php
+++ PHP/Herd/web-beginner/validator.php
@@ -6,4 +6,10 @@ class Validator
         $value = trim($value);
         return strlen($value) >= $min && strlen($value) <= $max;
     }
+
+    public static function email($value)
+    {
+        //validates the email
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
 }
