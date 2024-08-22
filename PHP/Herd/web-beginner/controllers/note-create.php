1eda8e4debbc47dad690205b005cdcdfece2d6df
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-22 19:13:49 +0300

    include a clean form from tailwind css

--- PHP/Herd/web-beginner/controllers/note-create.php
+++ PHP/Herd/web-beginner/controllers/note-create.php
@@ -8,5 +8,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 //Data Source Name (DSN)
 $config = require("config.php");
 $db = new Database($config['database'], 'root', 'Mh2x@WLM');
-
+ 
 require "views/note-create.view.php";
--- PHP/Herd/web-beginner/views/note-create.view.php
+++ PHP/Herd/web-beginner/views/note-create.view.php
@@ -4,24 +4,29 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello, welcome to creating new note page</p>
+        <form method="POST">
+            <div class="space-y-12">
+                <div class="border-b border-gray-900/10 pb-12">
+                    <h2 class="text-base font-semibold leading-7 text-gray-900">Adding a new note</h2>
 
-        <div class="mt-4">
-            <form method="POST">
-                <div class="mt-4 mb-4">
-                    <label for="body"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your note</label>
-                    <textarea id="body" name="body" rows="4"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write your thoughts here..."></textarea>
+                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="col-span-full">
+                            <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                            <div class="mt-2">
+                                <textarea id="body" name="body" rows="4"
+                                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-indigo-600 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Write your thoughts here..."></textarea>
+                            </div>
+                            <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences for your note...</p>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit"
-                    class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                    Publish note
-                </button>
-            </form>
 
-        </div>
+                <div class="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                </div>
+        </form>
     </div>
 </main>
 
