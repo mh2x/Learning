93c63220f910e224a97d1d689762e68377c8232d
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-22 15:20:14 +0300

    move DB example code to index.view.php

--- PHP/Herd/web-beginner/index.php
+++ PHP/Herd/web-beginner/index.php
@@ -3,26 +3,5 @@
 
 require_once("utils.php");
 require_once("database.php");
-//include("router.php");
+include("router.php");
 
-//connect to our MySQL database using PDO
-
-//Data Source Name (DSN)
-$config = require("config.php");
-
-
-$db = new Database($config['database'], 'root', 'Mh2x@WLM');
-//check if we're filtering by id
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $stmt = $db->query("select * from posts where id = ?", [$id]);
-    $post  = $stmt->fetch();
-    dd($post);
-} else {
-    $stmt = $db->query("select * from posts");
-    //fetch results as assoc array 
-    $posts  = $stmt->fetchAll();
-    foreach ($posts as $post) {
-        echo "<li>" . $post["title"] . "</li>";
-    }
-}
--- PHP/Herd/web-beginner/views/index.view.php
+++ PHP/Herd/web-beginner/views/index.view.php
@@ -8,6 +8,33 @@
         <h1 class="font-bold text-lg">Hello, Welcome to my home page.</h1>
 
     </div>
+    <div class="p-10 m-8 flex flex-col text-2xl">
+        <p class="text-blue-500 font-bold mb-4">Using MySQL via PDO Example:</p>
+
+        <?php
+        //connect to our MySQL database using PDO
+
+        //Data Source Name (DSN)
+        $config = require("config.php");
+        $db = new Database($config['database'], 'root', 'Mh2x@WLM');
+        //check if we're filtering by id
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $stmt = $db->query("select * from posts where id = ?", [$id]);
+            $post = $stmt->fetch();
+            dd($post);
+            echo "<li>" . $post["title"] . "</li>";
+        } else {
+            $stmt = $db->query("select * from posts");
+            //fetch results as assoc array
+            $posts = $stmt->fetchAll();
+            foreach ($posts as $post) {
+                echo "<li>" . $post["title"] . "</li>";
+            }
+        }
+        ?>
+
+    </div>
 </main>
 
 <?php require("partials/footer.php");  ?>
