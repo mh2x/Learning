0ded78736f4e60c691f2f89e114be6693d29a2a1
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-22 20:06:32 +0300

    implement create note form and insert values into DB, but with some security holes

--- PHP/Herd/web-beginner/controllers/note-create.php
+++ PHP/Herd/web-beginner/controllers/note-create.php
@@ -1,12 +1,24 @@
 <?php
 $heading = "Create Note";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
-}
-
 //Data Source Name (DSN)
 $config = require("config.php");
 $db = new Database($config['database'], 'root', 'Mh2x@WLM');
- 
+$currentUserId = 1; //hard-coded
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //dd($_POST);
+    $note = $_POST['body'];
+
+    $db->query(
+        'INSERT INTO notes(body, user_id) VALUES(:body,:user_id)',
+        [
+            'body' => $note,  //this is very risky, it can be anyting including <script>...</script> tags
+            'user_id' => $currentUserId
+        ]
+    );
+    
+}
+
+
 require "views/note-create.view.php";
