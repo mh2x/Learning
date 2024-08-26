3d27b7fbb2de90a85413d9cda039151b16c48d7d
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-26 16:33:48 +0300

    simple stuff

--- PHP/Herd/Tailwinder/resources/views/welcome.blade.php
+++ PHP/Herd/Tailwinder/resources/views/welcome.blade.php
@@ -16,21 +16,19 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
 
-<body class="m-10  w-full h-full flex flex-col items-center font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="flex flex-col items-center w-full h-full m-10 font-sans antialiased dark:bg-black dark:text-white/50">
     <div class="flex flex-col items-center bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <h1 class="text-violet-600 text-3xl font-bold m-10">
-            TailWinder v 1.0.0
+        <h1 class="text-3xl font-bold text-violet-600">
+            ✨✨ TailWinder ✨✨
         </h1>
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <div class="shrink-0">
-                <img class="size-12" src="/img/logo.svg" alt="ChitChat Logo">
-            </div>
-            <div>
-                <div class="text-xl font-medium text-black">ChitChat</div>
-                <p class="text-slate-500">You have a new message!</p>
+        <h2>v 1.0.0</h2>
+        <div class="flex items-center max-w-sm p-6 mx-auto mt-4 space-x-4 bg-white shadow-lg rounded-xl">
+            <div class="flex flex-col items-center p-5 m-5">
+                <div class="mb-4 text-2xl font-bold text-black">❤❤ Laravel ❤❤</div>
+                <p class="text-slate-500">Welcome to Tailwinder!</p>
             </div>
         </div>
-        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+        <footer class="py-16 text-sm text-center text-black dark:text-white/70">
             Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
         </footer>
     </div>
