70f5b60ceebc2bf61377a47edc8c3e4e3bb856ef
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-27 20:28:36 +0300

    Added counter component with some cool example

---
+++ PHP/Herd/LivewireBasics/resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>Count: {{ $count }}</h1> <button wire:click='increment'>Increment</button>
+
+    <h2 wire:click.window='$refresh'>Refresh page: click anywhere inside the window with click.window!</h2>
+    <!-- You can do throttling like this -->
+    <button wire:click.throttle.1000ms='increment'>Increment 2</button>
+</div>
--- PHP/Herd/LivewireBasics/resources/views/welcome.blade.php
+++ PHP/Herd/LivewireBasics/resources/views/welcome.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel with Livewire - The basics</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Laravel with Livewire - The basics</title>
 
-        <!-- Styles -->
-    </head>
-    <body>
-        <livewire:hello-world/>
-    </body>
-    </html>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+</head>
+
+<body>
+    <livewire:hello-world />
+    <livewire:counter />
+</body>
+
+</html>
