3fed546ea60c653343ae485b72b9dbd186e0fc88
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-28 05:39:36 +0300

    minor ui changes

--- PHP/Herd/LivewireDemo/resources/views/livewire/pages/todo/todo-header.blade.php
+++ PHP/Herd/LivewireDemo/resources/views/livewire/pages/todo/todo-header.blade.php
@@ -3,8 +3,9 @@
         <div class="flex-col items-center w-11/12 sm:w-2/3 lg:flex sm:mb-4">
             <h1
                 class="text-3xl font-black leading-10 text-center text-gray-800 sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl dark:text-white">
-                What do you want to do
-                <span class="text-violet-800 dark:text-green-500">Today?</span>
+                What do you want
+                <span class="text-violet-800 dark:text-green-500">todo</span>
+                Today?
             </h1>
             <p class="mt-5 mb-8 text-xl font-normal text-center text-gray-600 sm:mt-10 lg:w-10/12 dark:text-gray-300">
                 A Livewire TODO List by Mohammad Elhaj.
