db6af5f5baea2f89ab3b20f87c8a05dcff7fc62b
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-28 03:46:16 +0300

    TODO list design

---
+++ PHP/Herd/LivewireDemo/app/Livewire/TodoItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TodoItem extends Component
+{
+    public function render()
+    {
+        return view('livewire.todo-item');
+    }
+}
--- PHP/Herd/LivewireDemo/resources/views/todo.blade.php
+++ PHP/Herd/LivewireDemo/resources/views/todo.blade.php
@@ -10,8 +10,10 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __('Welcome to Livewire Demo!') }}
+                    {{ __('Welcome to Livewire TODO Demo!') }}
+                    <hr />
                 </div>
+                <livewire:todo-list />
             </div>
         </div>
     </div>
