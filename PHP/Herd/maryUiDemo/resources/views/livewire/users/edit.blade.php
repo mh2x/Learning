d9eeff95a36c3c6fa34cbcf3348803e28b971e0b
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-29 18:52:34 +0300

    upload avatar to user

--- PHP/Herd/maryUiDemo/resources/views/livewire/users/edit.blade.php
+++ PHP/Herd/maryUiDemo/resources/views/livewire/users/edit.blade.php
@@ -5,10 +5,11 @@ use App\Models\User;
 use Mary\Traits\Toast;
 use Livewire\Attributes\Rule;
 use App\Models\Country;
+use Livewire\WithFileUploads;
 
 new class extends Component {
     // We will use it later
-    use Toast;
+    use Toast, WithFileUploads;
 
     // Component parameter
     public User $user;
@@ -23,6 +24,9 @@ new class extends Component {
     // Optional
     #[Rule('sometimes')]
     public ?int $country_id = null;
+
+    #[Rule('nullable|image|max:1024')]
+    public $photo;
 
     // We also need this to fill Countries combobox on upcoming form
     public function with(): array
@@ -45,6 +49,13 @@ new class extends Component {
         // Update
         $this->user->update($data);
 
+        // Upload file and save the avatar `url` on User model
+        if ($this->photo) {
+            $url = $this->photo->store('users', 'public');
+            $this->user->update(['avatar' => "/storage/$url"]);
+            //$this->Info("User has new avatar at {{$url}}");
+        }
+
         // You can toast and redirect to any route
         $this->success('User updated with success.', redirectTo: '/users');
     }
@@ -53,10 +64,13 @@ new class extends Component {
 <div>
     <x-header title="Edit User '{{ $user->name }}'" separator progress-indicator />
     <x-form wire:submit="save">
+        <x-file label="Avatar" wire:model="photo" accept="image/png, image/jpeg">
+            <img src="{{ $user->avatar ?? '/empty-user.jpg' }}" class="h-32 rounded-lg" />
+        </x-file>
+
         <x-input label="Name" wire:model="name" />
         <x-input label="Email" wire:model="email" />
         <x-select label="Country" wire:model="country_id" :options="$countries" placeholder="---" />
-
         <x-slot:actions>
             <x-button label="Cancel" link="/users" />
             {{-- The important thing here is `type="submit"` --}}
