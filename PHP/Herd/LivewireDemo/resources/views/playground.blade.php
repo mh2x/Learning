56d1e819f2bb9a5b15ae27fe504f2071849336ea
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-29 06:54:24 +0300

    Added tabs

--- PHP/Herd/LivewireDemo/resources/views/playground.blade.php
+++ PHP/Herd/LivewireDemo/resources/views/playground.blade.php
@@ -19,7 +19,41 @@
                 <div>
                     <livewire:counter />
                 </div>
-
+            </div>
+        </div>
+        <div>
+            <script src="//unpkg.com/alpinejs"></script>
+            <div x-data="{
+                openTab: 1,
+                activeClasses: 'border-l border-t border-r rounded-t text-blue-700',
+                inactiveClasses: 'text-blue-500 hover:text-blue-700'
+            }" class="p-6">
+                <ul class="flex border-b">
+                    <li @click="openTab = 1" :class="{ '-mb-px': openTab === 1 }" class="-mb-px mr-1">
+                        <a href="#" :class="openTab === 1 ? activeClasses : inactiveClasses"
+                            class="bg-white inline-block py-2 px-4 font-semibold">
+                            Active
+                        </a>
+                    </li>
+                    <li @click="openTab = 2" :class="{ '-mb-px': openTab === 2 }" class="mr-1">
+                        <!-- Set active class by using :class provided by Alpine -->
+                        <a href="#" :class="openTab === 2 ? activeClasses : inactiveClasses"
+                            class="bg-white inline-block py-2 px-4 font-semibold">
+                            Tab
+                        </a>
+                    </li>
+                    <li @click="openTab = 3" :class="{ '-mb-px': openTab === 3 }" class="mr-1">
+                        <a href="#" :class="openTab === 3 ? activeClasses : inactiveClasses"
+                            class="bg-white inline-block py-2 px-4 font-semibold">
+                            Tab
+                        </a>
+                    </li>
+                </ul>
+                <div class="w-full">
+                    <div x-show="openTab === 1">Tab #1</div>
+                    <div x-show="openTab === 2">Tab #2</div>
+                    <div x-show="openTab === 3">Tab #3</div>
+                </div>
             </div>
         </div>
     </div>
