433fc661115e197315f844bbdb1d82a0317f362e
Mohammad Elhaj <mh2x@hotmail.com>
2024-08-29 18:08:52 +0300

    Added new classes for creating and editing users

---
+++ PHP/Herd/maryUiDemo/resources/views/livewire/index.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    //
+}; ?>
+
+<div>
+    <x-header title="Home" separator progress-indicator>
+        <x-slot:middle class="!justify-end">
+            <x-card>
+                <p>Add User Info...</p>
+            </x-card>
+        </x-slot:middle>
+    </x-header>
+
+</div>
