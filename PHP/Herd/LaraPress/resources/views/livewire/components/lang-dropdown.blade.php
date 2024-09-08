d2357a781a6699c108cbf85cd0744510595f8e65
Mohammad Elhaj <mh2x@hotmail.com>
2024-09-08 23:31:33 +0300

    Added checkmark next to default locale

--- PHP/Herd/LaraPress/resources/views/livewire/components/lang-dropdown.blade.php
+++ PHP/Herd/LaraPress/resources/views/livewire/components/lang-dropdown.blade.php
@@ -29,19 +29,27 @@ new class extends Component {
     <div tabindex="0" role="button" class="btn btn-ghost">
         <x-icon name="o-globe-alt" />
         {{ Str::upper($this->appLocale) }}
-        <svg class="inline w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+        <svg class="inline w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" aria-hidden="true" data-slot="icon">
             <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"></path>
         </svg>
     </div>
-    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-40 p-2 shadow">
+
         @foreach (getAllowedLocaleNames() as $key => $value)
             @if ($key === $this->appLocale)
-                <li class="text-slate-500 pointer-events-none"><a href="#">{{ __($value) }}</a></li>
+                <li class="text-slate-500 pointer-events-none">
+                    <a href="#">
+                        <span class="w-2">&#10003;</span>
+                        {{ __($value) }}</a>
+
+                </li>
             @else
-                <li><a href="#" wire:click.prevent="changeLocale('{{ $key }}')">{{ __($value) }}</a></li>
+                <li><a href="#" wire:click.prevent="changeLocale('{{ $key }}')">
+                        <span class="w-2"></span>
+                        {{ __($value) }}</a></li>
             @endif
         @endforeach
-
         <x-hr />
         <li><a href="/languages" class="text-yellow-500">
                 <x-icon name="s-cog-6-tooth" />
