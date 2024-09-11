8fd0be20677c36c2c394af02e2592b74e621c373
Mohammad Elhaj <mh2x@hotmail.com>
2024-09-11 05:07:52 +0300

    Good progress on saving translations

--- PHP/Herd/LaraPress/resources/views/pages/admin/languages.blade.php
+++ PHP/Herd/LaraPress/resources/views/pages/admin/languages.blade.php
@@ -14,7 +14,17 @@ new class extends Component {
     public $locales = [];
     public $table_headers = [];
     public $translationData = [];
-    public $edit_mode_id = 0;
+    public $edit_row_id = 0;
+    public $edit_translation = [];
+    public $showTranslationDialog = false;
+
+    //need to update the list items
+    protected $listeners = ['refreshItemList' => '$refresh'];
+
+    public function Refresh()
+    {
+        $this->dispatch('refreshItemList');
+    }
 
     // Reset pagination when any component property changes
     public function updated($property): void
@@ -100,22 +110,38 @@ new class extends Component {
 
     public function editRow($id)
     {
-        if ($this->edit_mode_id !== 0) {
+        if ($this->edit_row_id !== 0) {
             return;
         }
-        $this->edit_mode_id = $id;
+        $this->edit_row_id = $id;
+        $this->edit_translation = $this->translationData[$id];
+        //dd($this->edit_translation);
+        $this->showTranslationDialog = true;
     }
 
-    public function endEditRow()
+    public function endEditRow($save)
     {
-        $this->edit_mode_id = 0;
+        if ($save) {
+            $this->translationData[$this->edit_row_id] = $this->edit_translation;
+            //dd($this->translationData);
+            $this->Refresh();
+        }
+        //dd($this->edit_translation);
+        $this->showTranslationDialog = false;
+        $this->edit_row_id = 0;
     }
 
-    public function save()
+    public function saveLocales()
     {
         updateSettingsValue('allowed_locales', $this->app_locales ?? ['en']);
-        $this->success('Changes saved successfully.');
+        $this->success('New locales saved successfully.');
         //$names = getLocaleName($this->app_locales);
+    }
+
+    public function saveTranslations()
+    {
+        $this->edit_translation = [];
+        $this->success('Translations saved successfully.');
     }
 }; ?>
 
@@ -125,12 +151,12 @@ new class extends Component {
             <x-slot:title>
                 {{ __('Languages') }}
             </x-slot:title>
-            <x-form wire:submit="save" no-separator>
-                <div class="lg:grid grid-cols-6">
+            <x-form wire:submit="saveLocales" no-separator>
+                <div class="lg:grid grid-cols-9">
                     <div class="col-span-2">
                         <x-header title="Locales" subtitle="Choose locales you want to support" size="text-2xl" />
                     </div>
-                    <div class="col-span-2 grid gap-3">
+                    <div class="col-span-4 gap-3">
                         <x-choices-offline label="Supported Locales" wire:model="app_locales" :options="$locales" searchable
                             class="focus:text-red-500" />
                     </div>
@@ -138,7 +164,8 @@ new class extends Component {
                 <x-slot:actions>
                     {{-- The important thing here is `type="submit"` --}}
                     {{-- The spinner property is nice! --}}
-                    <x-button label="Save" icon="o-paper-airplane" spinner="save" type="submit" class="btn-primary" />
+                    <x-button label="Save Locales" icon="o-paper-airplane" spinner="save" type="submit"
+                        class="btn-primary" />
                 </x-slot:actions>
             </x-form>
             {{--  Translation section --}}
@@ -147,7 +174,7 @@ new class extends Component {
                 <div class="col-span-1">
                     <x-header title="Translations" subtitle="Localize your app messages" size="text-2xl" />
                 </div>
-                <div>
+                <x-form wire:submit='saveTranslations'>
                     <div class="overflow-x-auto">
                         <table class="table">
                             <!-- head -->
@@ -163,35 +190,54 @@ new class extends Component {
                                 <!-- row -->
                                 @foreach ($translations as $key => $translation)
                                     @php $index = 0; @endphp
-                                    <tr class="hover">
+                                    <tr class="hover" wire:click="editRow({{ $translation['#'] }})">
                                         @foreach ($translation as $value)
-                                            @if ($edit_mode_id === $translation['#'] && $index++ > 1)
-                                                <td><input type="text" value="{{ $value }}" class="text-black" />
-                                                </td>
-                                            @else
-                                                <td>{{ $value }}</td>
-                                            @endif
+                                            <td>{{ $value }}</td>
                                         @endforeach
-                                        @if ($edit_mode_id === 0)
-                                            <td><x-button icon="o-pencil-square" class="btn-circle btn-ghost"
-                                                    wire:click="editRow({{ $translation['#'] }})" /></td>
-                                        @else
-                                            @if ($edit_mode_id === $translation['#'])
-                                                <td><x-button icon="m-check-circle" class="btn-circle btn-ghost"
-                                                        wire:click="endEditRow" /></td>
-                                            @else
-                                                <td><x-button disabled icon="o-pencil-square" class="btn-circle btn-ghost"
-                                                        wire:click="editRow({{ $translation['#'] }})" /></td>
-                                            @endif
-                                        @endif
+                                        <td>
+                                            <x-button icon="o-pencil-square" class="btn-circle btn-ghost"
+                                                wire:click="editRow({{ $translation['#'] }})" />
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
+                    <x-slot:actions>
+                        {{-- The important thing here is `type="submit"` --}}
+                        {{-- The spinner property is nice! --}}
+                        <x-button label="Save Translations" icon="o-paper-airplane" spinner="save" type="submit"
+                            class="{{ empty($edit_translation) ? 'btn-disabled' : 'btn-error' }}" />
+                    </x-slot:actions>
+                </x-form>
+                <div class="mt-4 border border-gray-900 p-4">
                     {{ $translations->links() }}
                 </div>
             </div>
+            <!---- EDIT DIALOG -->
+            <x-modal wire:model="showTranslationDialog" title="Edit Translation" subtitle="edit translated text">
+                @php
+                    $index = 0;
+                @endphp
+                <x-form class="m-4 p-2">
+                    @foreach ($edit_translation as $key => $value)
+                        @if ($index < 2)
+                            <x-input label="{{ $headers[$index]['label'] }}" value="{{ $value }}" inline readonly
+                                class="text-gray-300" />
+                        @else
+                            <x-input label="{{ $headers[$index]['label'] }}"
+                                wire:model="edit_translation.{{ $key }}" />
+                        @endif
+                        @php
+                            $index++;
+                        @endphp
+                    @endforeach
+                    <x-slot:actions>
+                        <x-button label="Cancel" wire:click="endEditRow(false)" />
+                        <x-button label="Confirm" class="btn-warning" spinner="save" wire:click="endEditRow(true)" />
+                    </x-slot:actions>
+                </x-form>
+            </x-modal>
         </div>
     @endvolt
 </x-layouts.app>
