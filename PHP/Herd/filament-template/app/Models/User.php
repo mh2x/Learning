2b90f526314c0ff46010f4075074efffc76a65f7
Mohammad Elhaj <mh2x@hotmail.com>
2024-09-23 04:00:36 +0300

    integrated https://github.com/stephenjude/filament-jetstream

--- PHP/Herd/filament-template/app/Models/User.php
+++ PHP/Herd/filament-template/app/Models/User.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
-use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable implements FilamentUser /*,MustVerifyEmail*/
 {
     use HasApiTokens;
     use HasFactory;
--- PHP/Herd/filament-template/routes/web.php
+++ PHP/Herd/filament-template/routes/web.php
@@ -1,15 +1,36 @@
 <?php
 
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Support\Facades\Route;
+use Laravel\Jetstream\Http\Controllers\TeamInvitationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', fn() => view('welcome'));
+
+Route::redirect('/login', '/app/login')->name('login');
+
+Route::redirect('/register', '/app/register')->name('register');
+
+Route::redirect('/dashboard', '/app')->name('dashboard');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::redirect('/dashboard', '/app')->name('dashboard');
+    //Route::redirect('/dashboard', '/app')->name('dashboard');
 });
+
+Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
+    ->middleware(['signed', 'verified', 'auth', AuthenticateSession::class])
+    ->name('team-invitations.accept');
